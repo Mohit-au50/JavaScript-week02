3e32d58e4eaa487b8711030e7af55f1e3cd067b6
Mohit <109670799+Mohit-au50@users.noreply.github.com>
2023-08-04 21:16:27 +0530

    js codingChallenge day01

---
+++ day_01.js
@@ -0,0 +1,9 @@
+// Q Write an ARROW FUNCTION to print “Hello World”, 10 times, using for loop.
+
+// define the arrow function
+let arrowFunction = () => "Hello World!"
+
+// run the loop 10 time and call the function inside the loop
+for (let i = 0; i < 10; i++) {
+    console.log(arrowFunction())
+}
